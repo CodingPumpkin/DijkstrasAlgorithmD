9d509ffe1f88e00ad67d4081d350c8a327541621
Daniel Walker <tildaandpumpkins@gmail.com>
2024-09-26 16:57:25 +0300

    made some terrible life choices

--- source/app.d
+++ source/app.d
@@ -1,4 +1,5 @@
 import std.stdio;
+import std.algorithm;
 import graph;
 
 int minDistance(int[] dist, bool[] visited) {
@@ -31,11 +32,39 @@ void dijkstraAlgorithm(int[6][6] graph, int start) {
     }
 }
 
+void my_dijkstraAlgorithm(Graph graph, Vertex origin) {
+    int size = cast(int) graph.vertices.length;
+    int[char] distances;
+    foreach (vertex; graph.vertices)
+    	distances[vertex.name] = int.max; // creating a list to store the shortest distances for each vertex
+    distances[origin.name] = 0; // the distance from source to source is zero
+    char c = 'A';
+    for (int i = 0; i < size; i++)// vertex_name, vertex; graph.vertices) {
+    {
+		writeln(c);
+		Vertex current_vertex = graph.vertices[c];
+    	//char closest_vertex = vertex.shortest_connection();
+    	current_vertex.is_visited = true;
+        foreach (connection_name, length; current_vertex.connections) //for every connection this vertex has
+            if (!graph.vertices[connection_name].is_visited) //if it has not been visited yet
+            	if (distances[c] + length < distances[connection_name]) //
+            	{
+            		distances[connection_name] = distances[c] + length; 
+            	}
+        c++;
+	}
+    writeln("Vertex \t Distance from the Source");
+    foreach (name, _; graph.vertices){;
+        writeln(name, "\t\t", distances[name]);
+    }
+}
+
 int main()
 {
 	auto my_graph = new Graph();
 	my_graph.initDefaultGraph();
 	my_graph.printGraph();
+    my_dijkstraAlgorithm(my_graph, my_graph.get_vertex('C'));
     int[6][6] graph = [ [ 0, 1, 2, 0, 0, 0],
                         [ 1, 0, 1, 0, 3, 0],
                         [ 2, 1, 0, 2, 2, 0],
--- source/graph.d
+++ source/graph.d
@@ -1,14 +1,7 @@
 class Graph
 {
-private:
-	int size;
-	Vertex[] vertices;
-
 public:
-	this() 
-	{
-		size = 0;
-	}
+	Vertex[char] vertices;
 	void add_a_vertex(Vertex v)
 	{
 		auto ver = new Vertex();
@@ -17,62 +10,55 @@ public:
 		{
 			ver.connections[key] = value;
 		}
-		vertices ~= ver; 
-		size += 1;
+		vertices[v.name] = ver;
 	}
-	int get_size()
+	Vertex get_vertex(char c)
 	{
-		return size;
-	}
-	Vertex get_vertex(int i)
-	{
-		assert(i < size);
-		return vertices[i];
+		return vertices.require(c);
 	}
 	void initDefaultGraph()
 	{
 		auto v = new Vertex();
-		v.name = "A";
-		v.connections["A"] = 0;
-		v.connections["B"] = 1;
-		v.connections["C"] = 2;
+		v.name = 'A';
+		v.connections['A'] = 0;
+		v.connections['B'] = 1;
+		v.connections['C'] = 2;
 		add_a_vertex(v);
-		v.name = "B";
-		v.connections["A"] = 1;
-		v.connections["B"] = 0;
-		v.connections["C"] = 1;
-		v.connections["E"] = 3;
+		v.name = 'B';
+		v.connections['A'] = 1;
+		v.connections['B'] = 0;
+		v.connections['C'] = 1;
+		v.connections['E'] = 3;
 		add_a_vertex(v);
-		v.name = "C";
-		v.connections["A"] = 2;
-		v.connections["B"] = 1;
-		v.connections["C"] = 0;
-		v.connections["E"] = 2;
-		v.connections["D"] = 2;
+		v.name = 'C';
+		v.connections['A'] = 2;
+		v.connections['B'] = 1;
+		v.connections['C'] = 0;
+		v.connections['E'] = 2;
+		v.connections['D'] = 2;
 		add_a_vertex(v);
-		v.name = "D";
-		v.connections["D"] = 0;
-		v.connections["C"] = 2;
-		v.connections["E"] = 1;
-		v.connections["F"] = 1;
+		v.name = 'D';
+		v.connections['D'] = 0;
+		v.connections['C'] = 2;
+		v.connections['E'] = 1;
+		v.connections['F'] = 1;
 		add_a_vertex(v);
-		v.name = "E";
-		v.connections["B"] = 3;
-		v.connections["C"] = 2;
-		v.connections["D"] = 1;
-		v.connections["E"] = 0;
-		v.connections["F"] = 2;
+		v.name = 'E';
+		v.connections['B'] = 3;
+		v.connections['C'] = 2;
+		v.connections['D'] = 1;
+		v.connections['E'] = 0;
+		v.connections['F'] = 2;
 		add_a_vertex(v);
-		v.name = "F";
-		v.connections["D"] = 1;;
-		v.connections["E"] = 2;
-		v.connections["F"] = 0;
+		v.name = 'F';
+		v.connections['D'] = 1;;
+		v.connections['E'] = 2;
+		v.connections['F'] = 0;
 		add_a_vertex(v);
 	}
 	void printGraph()
 	{
 		import std.stdio;
-		writeln(size);
 		foreach (v; vertices)
 		{
 			writeln(v.name, " is connected to: ");
@@ -87,13 +73,25 @@ public:
 
 class Vertex
 {
-	int[string] connections;
-	string name;
-	bool is_visited = false;
 public:
-	void set_name(string n)
+	int[char] connections;
+	char name;
+	bool is_visited = false;
+	void set_name(char n)
 	{
 		name = n;
+	}
+	char shortest_connection()
+	{
+		int min = int.max;
+		char min_name = ' ';
+		foreach (key, value; connections)
+			if (value < min) 
+				{
+					min = value;
+					min_name = key;
+				}
+		return min_name;
 	}
 }
 
